3f2e69bf78979c97e255010c553113437395a766
Haytay <schabnowalisa@mail.ru>
2022-01-06 15:31:18 +0300

    Реализовано получение почты по протоколу POP3

--- fixture/application.py
+++ fixture/application.py
@@ -1,12 +1,15 @@
 from selenium import webdriver
 
+from fixture.james import JamesHelper
 from fixture.project import ProjectHelper
 from fixture.session import SessionHelper
+from fixture.signup import SighUpHelper
+from fixture.mail import MailHelper
 
 
 class Application:
 
-    def __init__(self, browser, base_url):
+    def __init__(self, browser, config):
         if browser == 'chrome':
             self.wd = webdriver.Chrome()
         elif browser == 'firefox':
@@ -17,7 +20,11 @@ class Application:
             raise ValueError("Unrecognized browser %s" % browser)
         self.session = SessionHelper(self)
         self.project = ProjectHelper(self)
-        self.base_url = base_url
+        self.james = JamesHelper(self)
+        self.signup = SighUpHelper(self)
+        self.mail = MailHelper(self)
+        self.config = config
+        self.base_url = config['web']['baseUrl']
 
     def is_valid(self):
         try:
