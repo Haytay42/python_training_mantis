c9365643522cf97fe05cf6107b7211dabbfb3b41
Haytay <schabnowalisa@mail.ru>
2022-01-06 15:45:39 +0300

    Добавлена проверка авторизации через SOAP

--- test/test_sign_up_new_account.py
+++ test/test_sign_up_new_account.py
@@ -4,7 +4,7 @@ import string
 
 def random_username(prefix, maxlen):
     symbols = string.ascii_letters
-    return prefix + "".join([random.choice(symbols) for i in range (random.randrange(maxlen))])
+    return prefix + "".join([random.choice(symbols) for i in range(random.randrange(maxlen))])
 
 
 def test_sign_up_new_account(app):
@@ -13,6 +13,4 @@ def test_sign_up_new_account(app):
     password = "test"
     app.james.ensure_user_exists(username, password)
     app.signup.new_user(username, email, password)
-    app.session.login(username, password)
-    assert app.session.is_logged_in_as(username)
-    app.session.logout()
+    assert app.soap.can_login(username, password)
